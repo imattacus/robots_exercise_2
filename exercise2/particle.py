2fe7e22b0d03cb89c96cf0eb13f0cecedd606acf
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2018-10-30 12:25:21 +0000

    Fill in noisy movement simulation for particle

--- exercise2/particle.py
+++ exercise2/particle.py
@@ -1,4 +1,6 @@
 from geometry_msgs.msg import Pose, Point, Quaternion
+import numpy as np
+
 
 class Particle(object):
     def __init__(self, x, y, rotation):
@@ -9,14 +11,32 @@ class Particle(object):
         self.rotation = Quaternion()
         self.rotation.x = rotation
 
+    def _noisy_update(self, current_value, increment, variance):
+        return current_value + increment + np.random.normal(loc=0, scale=variance)
+
     def estimate_update_position(self, transform):
-        # TODO: generate a new Particle, with position given by odometry
-        # model of type Twist (and a little bit of noise)
+        # TODO: work out best values for variance
+        x_variance = 1
+        y_variance = 1
+        rot_variance = 1
+
+        x_updated = self._noisy_update(current_value=self.position.x,
+                                       increment=transform.linear.x,
+                                       variance=x_variance)
+        y_updated = self._noisy_update(current_value=self.position.y,
+                                       increment=transform.linear.y,
+                                       variance=y_variance)
+        rot_updated = self._noisy_update(current_value=self.rotatation.x,
+                                         increment=transform.angular.z,
+                                         variance=rot_variance)
+
+        return Particle(x_updated, y_updated, rot_updated)
 
     def probability_of_pose(self, laser_scan, world_map):
         # TODO: calculate the probability that, given our current
         # pose estimate on world_map, we would get a laser scan that
         # looks like laser_scan
+        return 0.5
 
     def ros_pose(self):
         # Represent my position as ROS Pose Message class
