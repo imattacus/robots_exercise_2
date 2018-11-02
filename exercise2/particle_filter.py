fbf5cfa2043bc97fc55262a93a24540c69d94825
matt <mattcallaway1406@gmail.com>
2018-11-02 13:32:22 +0000

    sorted out odom topic

--- exercise2/particle_filter.py
+++ exercise2/particle_filter.py
@@ -2,14 +2,16 @@
 
 import numpy as np
 import rospy
-from geometry_msgs.msg import Twist, PoseArray
+from geometry_msgs.msg import TwistWithCovariance, Twist, PoseArray
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 from random import Random
 
 from particle import Particle
 from map import Map
 
 global particles
+global prev_tstamp
 
 def publish_particles():
     # Create and publish PoseArray for rviz
@@ -23,14 +25,21 @@ def publish_particles():
     particle_publisher.publish(pose_array)
 
 
-def move_particles(transform):
+def move_particles(msg):
     global particles
+	global prev_tstamp
+	
+	curr_tstamp = msg.header.stamp
+	x_vel = msg.twist.twist.linear.x
+	y_vel = msg.twist.twist.linear.y
+	rot_vel = msg.twist.twist.angular.z
+
     # Given the control signal we sent to the motors, update the
     # positions of all particles according to their motion model
-    new_particles = np.fromiter((particle.estimate_update_position(transform)
-                                 for particle in particles),
-                                particles.dtype)
+    new_particles = np.fromiter((particle.estimate_update_position(prev_tstamp, curr_tstamp, x_vel, y_vel, rot_vel) for particle in particles), particles.dtype)
+
     particles = new_particles
+	prev_tstamp = curr_tstamp
     publish_particles()
 
 
@@ -71,15 +80,17 @@ if __name__ == '__main__':
     global iteration
     global world_map
     global particle_count
+	global prev_tstamp
 
     particle_count = 5
     particles = None
     iteration = 0
     world_map = Map("~/catkin_ws/src/robots_exercise_2/stage/map.png")
+	prev_tstamp = rospy.Time.now()
 
     fresh_particles()
 
-    rospy.Subscriber('cmd_vel', Twist, move_particles)
+    rospy.Subscriber('odom', Odometry, move_particles)
     rospy.Subscriber('base_scan', LaserScan, resample_particles)
 
     particle_publisher = rospy.Publisher('particle_cloud',
